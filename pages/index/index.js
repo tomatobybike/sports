f5d786687bac8e54fb8469a4694da2950da60957
杨琼 <yangqiong@360.cn>
2018-05-19 12:00:43 +0800

    weixin

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,47 +1,197 @@
-var bmap = require('../../baidu/libs/bmap-wx.min.js');
-var wxMarkerData = [];
+var QQMapWX = require('../../libs/qqmap-wx-jssdk.min.js');
+var qqmapsdk;
 Page({
   data: {
     inputShowed: false,
     inputVal: "",
-    markers: [],
-    latitude: '',
-    longitude: '',
-    placeData: {}
+    latitude: 23.099994,
+    longitude: 113.324520,
+    markers: [{
+      id: 1,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      name: 'T.I.T 创意园'
+    },{
+      latitude: 23.099994,
+      longitude: 113.344520,
+      iconPath: '/assets/image/location.png'
+    }, {
+      latitude: 23.099994,
+      longitude: 113.304520,
+      iconPath: '/assets/image/location.png'
+    }],
+    covers: []
   },
-  makertap: function (e) {
-    var that = this;
-    var id = e.markerId;
-    that.showSearchInfo(wxMarkerData, id);
-    that.changeMarkerColor(wxMarkerData, id);
-  },
-  onLoad: function () {
-    var that = this;
-    var BMap = new bmap.BMapWX({
-      ak: 'tS7QkonVsVGh0VDdkIE2feekLGtDSGfu'
+  onShow: function (e) {
+    this.mapCtx = wx.createMapContext('myMap')
+    this.moveToLocation()
+    //this.includePoints()
+    // 调用接口
+    qqmapsdk.search({
+      keyword: '酒店',
+      success: function (res) {
+        console.log(res);
+      },
+      fail: function (res) {
+        console.log(res);
+      },
+      complete: function (res) {
+        console.log(res);
+      }
     });
-    var fail = function (data) {
-      console.log(data)
-    };
-    var success = function (data) {
-      wxMarkerData = data.wxMarkerData;
-      that.setData({
-        markers: wxMarkerData
+  },
+  onLoad: function (options) {
+    // 实例化API核心类
+    qqmapsdk = new QQMapWX({
+      key: 'P3JBZ-WX36G-3K4QJ-ITACJ-BT42Z-6OBZR'
+    });
+    // 3.设置地图控件的位置及大小，通过设备宽高定位
+    wx.getSystemInfo({
+      success: (res) => {
+        this.setData({
+          controls: [{
+            id: 1,
+            iconPath: '/assets/image/map/location.png',
+            position: {
+              left: 20,
+              top: res.windowHeight - 120,
+              width: 50,
+              height: 50
+            },
+            clickable: true
+          },
+          {
+            id: 3,// 足球
+            iconPath: '/assets/image/football.png',
+            position: {
+              left: res.windowWidth - 70,
+              top: 150,
+              width: 45,
+              height: 45
+            },
+            clickable: true
+          },
+          {
+            id: 4,
+            iconPath: '/assets/image/map/marker.png',
+            position: {
+              left: res.windowWidth / 2 - 11,
+              top: res.windowHeight / 2 - 45,
+              width: 22,
+              height: 45
+            },
+            clickable: true
+          },
+          {
+            id: 5,// 篮球
+            iconPath: '/assets/image/basketball.png',
+            position: {
+              left: res.windowWidth - 68,
+              top: 50,
+              width: 45,
+              height: 45
+            },
+            clickable: true
+          }]
+        })
+      }
+    });
+  },
+  // 地图控件点击事件
+  bindcontroltap: function (e) {
+    // 判断点击的是哪个控件 e.controlId代表控件的id，在页面加载时的第3步设置的id
+    switch (e.controlId) {
+      // 点击定位控件
+      case 1: this.moveToLocation();
+        break;
+      // 点击立即用车，判断当前是否正在计费
+      case 2: if (this.timer === "" || this.timer === undefined) {
+        // 没有在计费就扫码
+        wx.scanCode({
+          success: (res) => {
+            // 正在获取密码通知
+            wx.showLoading({
+              title: '正在获取密码',
+              mask: true
+            })
+            // 请求服务器获取密码和车号
+            wx.request({
+              url: 'https://www.easy-mock.com/mock/59098d007a878d73716e966f/ofodata/password',
+              data: {},
+              method: 'GET',
+              success: function (res) {
+                // 请求密码成功隐藏等待框
+                wx.hideLoading();
+                // 携带密码和车号跳转到密码页
+                wx.redirectTo({
+                  url: '../scanresult/index?password=' + res.data.data.password + '&number=' + res.data.data.number,
+                  success: function (res) {
+                    wx.showToast({
+                      title: '获取密码成功',
+                      duration: 1000
+                    })
+                  }
+                })
+              }
+            })
+          }
+        })
+        // 当前已经在计费就回退到计费页
+      } else {
+        wx.navigateBack({
+          delta: 1
+        })
+      }
+        break;
+      // 点击保障控件，跳转到报障页
+      case 3: wx.navigateTo({
+        url: '../warn/index'
       });
-      that.setData({
-        latitude: wxMarkerData[0].latitude
+        break;
+      // 点击头像控件，跳转到个人中心
+      case 5: wx.navigateTo({
+        url: '../my/index'
       });
-      that.setData({
-        longitude: wxMarkerData[0].longitude
-      });
+        break;
+      default: break;
     }
-    BMap.search({
-      "query": '美食',
-      fail: fail,
-      success: success,
-      iconPath: '../../baidu/img/marker_red.png',
-      iconTapPath: '../../baidu/img/marker_red.png'
-    });
+  },
+  getCenterLocation: function () {
+    this.mapCtx.getCenterLocation({
+      success: function (res) {
+        console.log(res.longitude)
+        console.log(res.latitude)
+      }
+    })
+  },
+  moveToLocation: function () {
+    this.mapCtx.moveToLocation()
+  },
+  translateMarker: function () {
+    this.mapCtx.translateMarker({
+      markerId: 1,
+      autoRotate: true,
+      duration: 1000,
+      destination: {
+        latitude: 23.10229,
+        longitude: 113.3345211,
+      },
+      animationEnd() {
+        console.log('animation end')
+      }
+    })
+  },
+  includePoints: function () {
+    this.mapCtx.includePoints({
+      padding: [10],
+      points: [{
+        latitude: 23.10229,
+        longitude: 113.3345211,
+      }, {
+        latitude: 23.00229,
+        longitude: 113.3345211,
+      }]
+    })
   },
   showSearchInfo: function (data, i) {
     var that = this;
@@ -53,21 +203,9 @@ Page({
       }
     });
   },
-  changeMarkerColor: function (data, id) {
-    var that = this;
-    var markersTemp = [];
-    for (var i = 0; i < data.length; i++) {
-      if (i === id) {
-        data[i].iconPath = "../../baidu/img/marker_yellow.png";
-      } else {
-        data[i].iconPath = "../../baidu/img/marker_red.png";
-      }
-      markersTemp[i] = data[i];
-    }
-    that.setData({
-      markers: markersTemp
-    });
-  },
+
+
+
   showInput: function () {
     this.setData({
       inputShowed: true
